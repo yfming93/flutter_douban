a4662d9b7711bf6d1c282340e3c33bb680616941
袁凤鸣 <yfmingo@163.com>
2020-07-24 11:15:07 +0800

    add：nil

--- lib/service/home_request.dart
+++ lib/service/home_request.dart
@@ -1,10 +1,12 @@
 import 'package:flutter_douban/model/home_model.dart';
 import 'package:flutter_douban/service/http_request.dart';
+import 'package:flutter_douban/utils/fmlog.dart';
 import 'config.dart';
 
 class HomeRequest {
   static Future<List<MovieItem>> requestMovieList (int start) async {
     final movieUrl = "/movie/top250?apikey=${HomeConfig.apikey}&start=$start&count=${HomeConfig.movieCount}";
+
     final result = await HttpRequest.request(movieUrl);
     final subjects = result["subjects"];
     List<MovieItem> movies = [];
--- lib/utils/fmlog.dart
+++ lib/utils/fmlog.dart
@@ -1,8 +1,56 @@
-void fmlog(Object message, StackTrace current) {
-  FMCustomTrace programInfo = FMCustomTrace(current);
-  int isDebug = 1;
-  if (isDebug == 1){
-    print("所在文件: ${programInfo.fileName}, 所在行: ${programInfo.lineNumber}, 打印信息: $message");
+import 'package:dio/dio.dart';
+import 'package:flutter/cupertino.dart';
+import 'dart:convert';
+
+enum DevStatus { kReleaseMode, kProfileMode, kDebugMode }
+
+
+void fmlog(Object message, StackTrace trace, {Response response}) {
+  DevStatus status = DevStatus.kDebugMode;
+  if (status == DevStatus.kDebugMode) {
+    //
+    FMCustomTrace programInfo = FMCustomTrace(trace);
+    if (programInfo != null &&
+        programInfo.fileName != null &&
+        programInfo.lineNumber != null) {
+
+      if (response == null) {
+
+        print(
+            "所在文件: ${programInfo.fileName}, 所在行: ${programInfo.lineNumber}, 打印信息: $message");
+      }else{
+        Map httpLogMap = Map();
+        httpLogMap.putIfAbsent("requestUrl", () => "${response.request.uri}");
+        httpLogMap.putIfAbsent("requestHeaders", () => response.request.headers);
+        httpLogMap.putIfAbsent(
+            "requestQueryParameters", () => response.request.queryParameters);
+        httpLogMap.putIfAbsent("respondData", () => response.data);
+
+        //带有首行缩进的Json格式
+        JsonEncoder encoder = JsonEncoder.withIndent('  ');
+
+        try {
+          var encoderString = encoder.convert(httpLogMap);
+          var outString = "所在文件: ${programInfo.fileName}, 所在行: ${programInfo.lineNumber}, 打印信息: $encoderString";
+
+          // print(encoderString);
+          // 不使用print()方法是因为这是单条输出，如果过长无法显示全
+          // 所以使用debugPrint()
+          debugPrint(outString);
+          // 下面这语句的效果与debugPrint 相同
+          //encoderString.split('\n').forEach((element) => print(element));
+        } catch (e) {
+          print(e);
+        }
+      }
+
+    } else {
+      print("打印信息:$message");
+    }
+  } else if (status == DevStatus.kReleaseMode) {
+    print("----ReleaseMode-------");
+  } else if (status == DevStatus.kProfileMode) {
+    print("----ProfileMode-------");
   }
 }
 
